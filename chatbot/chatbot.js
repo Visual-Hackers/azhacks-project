32e7d6d910bfdadec0ef4ba079ca84059dc9f196
Carlos <guerreroc47@gmail.com>
2020-01-19 06:13:09 -0700

    Fix mongoose

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -2,6 +2,7 @@ const dialogflow = require("dialogflow");
 const structjson = require("./structjson");
 const config = require("../config/keys");
 const projectID = config.googleProjectID;
+const mongoose = require('mongoose');
 const credentials = {
     client_email: config.googleClientEmail,
     private_key: config.googlePrivateKey
