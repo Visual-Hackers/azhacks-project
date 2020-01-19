7ddc22bff41fdcc11c652af1d88841a02450fc06
Carlos <guerreroc47@gmail.com>
2020-01-19 05:00:58 -0700

    added the modules for npm

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,8 +1,12 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import axios from 'axios/index';
 
 import Cookies from 'universal-cookie';
-import { v4 as uuid } from 'uuid';
+import {
+    v4 as uuid
+} from 'uuid';
 
 import Message from './Message';
 import Card from './Card';
@@ -23,7 +27,9 @@ class Chatbot extends Component {
             messages: []
         };
         if (cookies.get('userID') === undefined) {
-            cookies.set('userID', uuid(), { path: '/' });
+            cookies.set('userID', uuid(), {
+                path: '/'
+            });
         }
     }
 
@@ -37,8 +43,13 @@ class Chatbot extends Component {
             }
         };
 
-        this.setState({messages: [...this.state.messages, says]});
-        const res = await axios.post('/api/df_text_query',  {text: queryText, userID: cookies.get('userID')});
+        this.setState({
+            messages: [...this.state.messages, says]
+        });
+        const res = await axios.post('/api/df_text_query', {
+            text: queryText,
+            userID: cookies.get('userID')
+        });
 
         for (let msg of res.data.fulfillmentMessages) {
             console.log(JSON.stringify(msg));
@@ -46,19 +57,26 @@ class Chatbot extends Component {
                 speaks: 'bot',
                 msg: msg
             }
-            this.setState({ messages: [...this.state.messages, says]});
+            this.setState({
+                messages: [...this.state.messages, says]
+            });
         }
     };
 
     async df_event_query(eventName) {
-        const res = await axios.post('/api/df_event_query',  {event: eventName, userID: cookies.get('userID')});
+        const res = await axios.post('/api/df_event_query', {
+            event: eventName,
+            userID: cookies.get('userID')
+        });
 
         for (let msg of res.data.fulfillmentMessages) {
             let says = {
                 speaks: 'bot',
                 msg: msg
             };
-            this.setState({ messages: [...this.state.messages, says]});
+            this.setState({
+                messages: [...this.state.messages, says]
+            });
         }
     }
 
@@ -67,69 +85,145 @@ class Chatbot extends Component {
     }
 
     componentDidUpdate() {
-        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+        this.messagesEnd.scrollIntoView({
+            behavior: "smooth"
+        });
         this.talkInput.focus();
     }
 
     renderCards(cards) {
-        return cards.map((card, i) => <Card key={i} payload={card.structValue}/>);
-    }
-
-    renderOneMessage(message, i) {
-
-        if (message.msg && message.msg.text && message.msg.text.text) {
-            return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
-        } else if (message.msg && message.msg.payload.fields.cards) { //message.msg.payload.fields.cards.listValue.values
-
-            return <div key={i}>
-                <div className="card-panel grey lighten-5 z-depth-1">
-                    <div style={{overflow: 'hidden'}}>
-                        <div className="col s2">
-                            <a href="/" className="btn-floating btn-large waves-effect waves-light red">{message.speaks}</a>
-                        </div>
-                        <div style={{ overflow: 'auto', overflowY: 'scroll'}}>
-                            <div style={{ height: 300, width:message.msg.payload.fields.cards.listValue.values.length * 270}}>
-                                {this.renderCards(message.msg.payload.fields.cards.listValue.values)}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        return cards.map((card, i) => < Card key = {
+                i
+            }
+            payload = {
+                card.structValue
+            }
+            />);
         }
-    }
 
-    renderMessages(returnedMessages) {
-        if (returnedMessages) {
-            return returnedMessages.map((message, i) => {
-                return this.renderOneMessage(message, i);
+        renderOneMessage(message, i) {
+
+            if (message.msg && message.msg.text && message.msg.text.text) {
+                return <Message key = {
+                    i
                 }
-            )
-        } else {
-            return null;
+                speaks = {
+                    message.speaks
+                }
+                text = {
+                    message.msg.text.text
+                }
+                />;
+            } else if (message.msg && message.msg.payload.fields.cards) { //message.msg.payload.fields.cards.listValue.values
+
+                return <div key = {
+                        i
+                    } >
+                    <
+                    div className = "card-panel grey lighten-5 z-depth-1" >
+                    <
+                    div style = {
+                        {
+                            overflow: 'hidden'
+                        }
+                    } >
+                    <
+                    div className = "col s2" >
+                    <
+                    a href = "/"
+                className = "btn-floating btn-large waves-effect waves-light red" > {
+                        message.speaks
+                    } < /a> <
+                    /div> <
+                    div style = {
+                        {
+                            overflow: 'auto',
+                            overflowY: 'scroll'
+                        }
+                    } >
+                    <
+                    div style = {
+                        {
+                            height: 300,
+                            width: message.msg.payload.fields.cards.listValue.values.length * 270
+                        }
+                    } > {
+                        this.renderCards(message.msg.payload.fields.cards.listValue.values)
+                    } <
+                    /div> <
+                    /div> <
+                    /div> <
+                    /div> <
+                    /div>
+            }
+        }
+
+        renderMessages(returnedMessages) {
+            if (returnedMessages) {
+                return returnedMessages.map((message, i) => {
+                    return this.renderOneMessage(message, i);
+                })
+            } else {
+                return null;
+            }
+        }
+
+        _handleInputKeyPress(e) {
+            if (e.key === 'Enter') {
+                this.df_text_query(e.target.value);
+                e.target.value = '';
+            }
+        }
+
+        render() {
+            return ( <
+                div style = {
+                    {
+                        height: 400,
+                        width: 400,
+                        float: 'right'
+                    }
+                } >
+                <
+                div id = "chatbot"
+                style = {
+                    {
+                        height: '100%',
+                        width: '100%',
+                        overflow: 'auto'
+                    }
+                } >
+                <
+                h2 > Chatbot < /h2> {
+                    this.renderMessages(this.state.messages)
+                } <
+                div ref = {
+                    (el) => {
+                        this.messagesEnd = el;
+                    }
+                }
+                style = {
+                    {
+                        float: "left",
+                        clear: "both"
+                    }
+                } >
+                <
+                /div> <
+                input type = "text"
+                ref = {
+                    (input) => {
+                        this.talkInput = input;
+                    }
+                }
+                onKeyPress = {
+                    this._handleInputKeyPress
+                }
+                /> <
+                /div> <
+                /div>
+            );
         }
     }
 
-    _handleInputKeyPress(e) {
-        if (e.key === 'Enter') {
-            this.df_text_query(e.target.value);
-            e.target.value = '';
-        }
-    }
-    
-    render() {
-        return (
-            <div style={{ height: 400, width: 400, float: 'right' }}>
-                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto'}}>
-                    <h2>Chatbot</h2>
-                    {this.renderMessages(this.state.messages)}
-                    <div ref={(el) => { this.messagesEnd = el; }}
-                         style={{ float:"left", clear: "both" }}>
-                    </div>
-                    <input type="text" ref={(input) => { this.talkInput = input; }}  onKeyPress={this._handleInputKeyPress}  />
-                </div>
-            </div>
-        );
-    }
-}
-
-export default Chatbot;
+    export default Chatbot;
